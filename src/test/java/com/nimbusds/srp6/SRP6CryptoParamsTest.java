c00918eee7206ebe313a216eb9f959c6b41fc834
Simon Massey <simon@simonmassey.org>
2019-10-27 20:48:41 +0000

    tests that demo issue 23 with the primes currently on master

--- src/test/java/com/nimbusds/srp6/SRP6CryptoParamsTest.java
+++ src/test/java/com/nimbusds/srp6/SRP6CryptoParamsTest.java
@@ -1,27 +1,79 @@
 package com.nimbusds.srp6;
 
-
 import java.math.BigInteger;
-
 import junit.framework.TestCase;
-
 
 /**
  * Tests the SRP6 crypto params.
  */
 public class SRP6CryptoParamsTest extends TestCase {
 
+	private final byte[] password = "password".getBytes();
 
-	public void testPrecomputedPrimes() {
+	public void testN256() {
 
 		assertTrue(SRP6CryptoParams.N_256.isProbablePrime(15));
-		assertTrue(SRP6CryptoParams.N_512.isProbablePrime(15));
-		assertTrue(SRP6CryptoParams.N_768.isProbablePrime(15));
-		assertTrue(SRP6CryptoParams.N_1024.isProbablePrime(15));
-		assertTrue(SRP6CryptoParams.N_1536.isProbablePrime(15));
-		assertTrue(SRP6CryptoParams.N_2048.isProbablePrime(15));
+
+		SRP6VerifierGenerator generator = new SRP6VerifierGenerator(SRP6CryptoParams.getInstance(512, "SHA-512"));
+		byte[] salt;
+		for (int i = 0; i < 100; i++) {
+			salt = generator.generateRandomSalt();
+			assertEquals(16, salt.length);
+			assertEquals(64, generator.generateVerifier(salt, password).toByteArray().length);
+		}
 	}
 
+	public void testN512() {
+
+		assertTrue(SRP6CryptoParams.N_512.isProbablePrime(15));
+
+		SRP6VerifierGenerator generator = new SRP6VerifierGenerator(SRP6CryptoParams.getInstance(512, "SHA-512"));
+		byte[] salt;
+		for (int i = 0; i < 100; i++) {
+			salt = generator.generateRandomSalt();
+			assertEquals(16, salt.length);
+			assertEquals(64, generator.generateVerifier(salt, password).toByteArray().length);
+		}
+	}
+
+	public void testN768() {
+
+		assertTrue(SRP6CryptoParams.N_768.isProbablePrime(15));
+
+		SRP6VerifierGenerator generator = new SRP6VerifierGenerator(SRP6CryptoParams.getInstance(768, "SHA-512"));
+		byte[] salt;
+		for (int i = 0; i < 100; i++) {
+			salt = generator.generateRandomSalt();
+			assertEquals(16, salt.length);
+			assertEquals(96, generator.generateVerifier(salt, password).toByteArray().length);
+		}
+	}
+
+	public void testN1024() {
+
+		assertTrue(SRP6CryptoParams.N_1024.isProbablePrime(15));
+
+		SRP6VerifierGenerator generator = new SRP6VerifierGenerator(SRP6CryptoParams.getInstance(1024, "SHA-512"));
+		byte[] salt;
+		for (int i = 0; i < 100; i++) {
+			salt = generator.generateRandomSalt();
+			assertEquals(16, salt.length);
+			assertEquals(128, generator.generateVerifier(salt, password).toByteArray().length);
+		}
+	}
+
+	public void testN1536() {
+
+		assertTrue(SRP6CryptoParams.N_1536.isProbablePrime(15));
+
+		SRP6VerifierGenerator generator = new SRP6VerifierGenerator(SRP6CryptoParams.getInstance(1536, "SHA-512"));
+		byte[] salt;
+		for (int i = 0; i < 100; i++) {
+			salt = generator.generateRandomSalt();
+			assertEquals(16, salt.length);
+			assertEquals(192, generator.generateVerifier(salt, password).toByteArray().length);
+		}
+	}
 
 	public void testIllegalGeneratorArg() {
 
@@ -54,3 +106,4 @@ public class SRP6CryptoParamsTest extends TestCase {
 		}
 	}
 }
+0
